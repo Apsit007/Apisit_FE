3ea9c8791875f2bd60ab8896f886bcf137c591c2
apisit:name <apisit@multita.co.th>
2024-07-11 23:43:50 +0700

    update:  cv down load

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,8 +6,20 @@ interface Props {
     VisitorName: string;
     VisitorCount: number;
 }
+interface Props {
+    VisitorName: string;
+    VisitorCount: number;
+}
 function Header({ VisitorName, VisitorCount }: Props) {
-
+    const handleDownload = () => {
+        const pdfUrl = 'https://github.com/Apsit007/Apisit_introduce.github.io/raw/master/src/assets/Apisit%20Purisan.pdf'; // Replace with the actual PDF URL
+        const link = document.createElement('a');
+        link.href = pdfUrl;
+        link.download = 'Apisit_Purisan.pdf'; // Specify the desired downloaded file name
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
     return (
         <>
             <div className='navbar d-flex justify-content-between p-3'>
@@ -16,7 +28,7 @@ function Header({ VisitorName, VisitorCount }: Props) {
                     <div>
                         <FontAwesomeIcon icon={faUser} />   {VisitorCount}
                     </div>
-                    <p>CV </p>
+                    <p onClick={handleDownload} style={{ cursor: 'pointer' }}>CV</p>
                 </div>
             </div>
         </>
--- src/components/aboutme/aboutme.tsx
+++ src/components/aboutme/aboutme.tsx
@@ -4,10 +4,11 @@ function Aboutme() {
     return (
         <div className=' d-flex justify-content-center pt-5'>
 
-            <p className='aboutme text-center'>"Hello, I'm a full-stack developer with expertise in both front-end and back-end development. With a proven track record of efficiently developing diverse and user-friendly applications, I specialize in programming languages such as JavaScript, TypeScript, C#, HTML, and CSS. I have experience working with MVC models and designing databases to ensure optimal system performance.
-                If you're looking for a developer capable of handling both web front-end and back-end responsibilities, I'm ready to contribute and turn your project into a reality!"</p>
+            <p className='aboutme text-center'>"Hello, I'm a full-stack solfwere developer with expertise in both front-end and back-end development. With a proven track record of efficiently developing diverse and user-friendly applications, I specialize in programming languages such as JavaScript, TypeScript, C#, HTML, and CSS. I have experience working with MVC models and designing databases to ensure optimal system performance.
+                If you're looking for a developer capable of handling both web front-end and back-end responsibilities, I'm ready to contribute and turn your project into a reality."</p>
         </div>
     )
 }
+
 
 export default Aboutme
